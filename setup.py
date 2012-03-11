99b4623048cafcd0c0b1ed5d0461b2d563a2ee79
Jordan McCoy <mccoy.jordan@gmail.com>
2012-03-11 12:00:45 -0500

    a bit of housekeeping

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='mesh',
-    version='1.0.a1',
+    version='1.0.0a1',
     description='A declarative RESTful API framework.',
     author='Jordan McCoy',
     author_email='mccoy.jordan@gmail.com',
     license='BSD',
-    url='http://github.com/jordanm/mesh',
+    url='http://github.com/siq/mesh',
     packages=[
         'mesh',
         'mesh.documentation',
@@ -15,4 +15,14 @@ setup(
         'mesh.standard',
         'mesh.transport',
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
