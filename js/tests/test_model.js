408223384d1848c25b44ae889fd03ebe9149d557
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-06-19 17:15:33 -0500

    added test cases for the two previous commits

--- js/tests/test_model.js
+++ js/tests/test_model.js
@@ -2,58 +2,31 @@
 define([
     'vendor/underscore',
     'vendor/jquery',
+    'bedrock/uuid',
     './../request',
-    './example'
-], function(_, $, Request, Example) {
-    var manager = Example.models, last_ajax_call;
-
-    var ajax_failed = function() {
-        ok(false, 'ajax request failed');
-        start();
-    };
-
-    module('models', {
-        setup: function() {
-            Request.prototype.ajax = function(params) {
-                var dfd = $.Deferred();
-                last_ajax_call = params;
-                setTimeout(function() {
-                    var ret;
-                    if (params.type !== 'POST' && params.type !== 'DELETE') {
-                        if (/2$/.test(params.url)) {
-                            ret = {"default_field": 1, "required_field": "test", "id": 2};
-                        } else {
-                            ret = {"total": 1, "resources": [{"default_field": 1, "required_field": "test", "id": 1}]};
-                        }
-                    } else {
-                        ret = {"id": 2};
-                    }
-                    dfd.resolve(ret);
-                    params.success(ret, 200, {});
-                }, 0);
-                // return $.ajax(params);
-                return dfd;
-            };
-        },
-        teardown: function() {
-            Request.ajax = $.ajax;
-        }
-    });
+    './example',
+    './examplewithuuid'
+], function(_, $, uuid, Request, Example, ExampleWithUuid) {
+    var manager = Example.models,
+        ajaxFailed = function() {
+            ok(false, 'ajax request failed');
+            start();
+        };
 
     test('instantiation', function() {
         var model = Example();
-        strictEqual(model.id, null);
+        ok(model.id === null);
         ok(_.isString(model.cid) && model.cid.length > 0);
         deepEqual(model._changes, {});
         ok(!model._loaded);
-        strictEqual(model._manager, model.__models__);
-        strictEqual(manager.models[model.cid], model);
+        ok(model._manager === model.__models__);
+        ok(manager.models[model.cid] === model);
 
         var same_model = manager.get(model.cid);
-        strictEqual(same_model, model);
+        ok(same_model === model);
 
         model = Example({integer_field: 2});
-        strictEqual(model.integer_field, 2);
+        ok(model.integer_field === 2);
         deepEqual(model._changes, {});
     });
 
@@ -62,82 +35,106 @@ define([
         ok(!model.has('integer_field'));
 
         var retval = model.set('integer_field', 2);
-        strictEqual(retval, model);
+        ok(retval === model);
 
         ok(model.has('integer_field'));
-        strictEqual(model.integer_field, 2);
-        strictEqual(model.html('integer_field'), '2');
+        ok(model.integer_field === 2);
+        ok(model.html('integer_field') === '2');
         deepEqual(model._changes, {integer_field: true});
 
         model.set({integer_field: 4, text_field: 'text'});
-        strictEqual(model.integer_field, 4);
-        strictEqual(model.text_field, 'text');
+        ok(model.integer_field === 4);
+        ok(model.text_field === 'text');
         deepEqual(model._changes, {integer_field: true, text_field: true});
     });
 
     test('change events', function() {
         var model = Example(), calls = 0;
         model.on('change', function(event, changed_model, changes) {
-            strictEqual(event, 'change');
-            strictEqual(changed_model, model);
-            strictEqual(model.integer_field, 2);
+            ok(event === 'change');
+            ok(changed_model === model);
+            ok(model.integer_field === 2);
             deepEqual(changes, {integer_field: true});
             calls++;
         });
 
-        strictEqual(calls, 0);
+        ok(calls === 0);
         model.set('integer_field', 2);
-        strictEqual(calls, 1);
+        ok(calls === 1);
 
         model.set('integer_field', 2);
-        strictEqual(calls, 1);
+        ok(calls === 1);
     });
 
     asyncTest('lifecycle', function() {
-        var model = Example({required_field: 'test'});
-        strictEqual(model.id, null);
+        var lastAjaxCall = null,
+            oldAjax = Request.ajax(function(params) {
+                var dfd = $.Deferred();
+                lastAjaxCall = params;
+                setTimeout(function() {
+                   var ret;
+                   if (params.type !== 'POST' && params.type !== 'DELETE') {
+                       if (/2$/.test(params.url)) {
+                           ret = {"default_field": 1, "required_field": "test", "id": 2};
+                       } else {
+                           ret = {"total": 1, "resources": [{"default_field": 1, "required_field": "test", "id": 1}]};
+                       }
+                   } else {
+                       ret = {"id": 2};
+                   }
+                   dfd.resolve(ret);
+                   params.success(ret, 200, {});
+                }, 0);
+                return dfd;
+            }),
+            model = Example({required_field: 'test'});
+
+        ok(model.id === null);
         ok(!model._loaded);
 
         model.save().then(function(saved_model) {
             var id = model.id;
-            strictEqual(saved_model, model);
+            ok(saved_model === model);
             ok(model.id);
             ok(model._loaded);
-            strictEqual(manager.models[model.id], model);
+            ok(manager.models[model.id] === model);
             deepEqual(model._changes, {});
 
             manager.clear();
             model = manager.get(id);
             ok(model);
-            strictEqual(model.id, id);
-            strictEqual(model.cid, null);
+            ok(model.id === id);
+            ok(model.cid === null);
             ok(!model.has('required_field'));
 
             model.refresh().then(function(refreshed_model) {
-                strictEqual(refreshed_model, model);
-                strictEqual(model.required_field, 'test');
+                ok(refreshed_model === model);
+                ok(model.required_field === 'test');
 
                 model.set('text_field', 'text');
                 model.save().then(function() {
-                    deepEqual($.parseJSON(last_ajax_call.data), {text_field: 'text'});
-                    strictEqual(model.text_field, 'text');
+                    deepEqual($.parseJSON(lastAjaxCall.data), {text_field: 'text'});
+                    ok(model.text_field === 'text');
                     deepEqual(model._changes, {});
 
                     model.destroy().then(function(response) {
                         deepEqual(response, {id: model.id});
-                        strictEqual(manager.models[model.id], undefined);
+                        ok(manager.models[model.id] === undefined);
+                        Request.ajax(oldAjax);
                         start();
-                    }, ajax_failed);
-                }, ajax_failed);
-            }, ajax_failed);
-        }, ajax_failed);
+                    }, ajaxFailed);
+                }, ajaxFailed);
+            }, ajaxFailed);
+        }, ajaxFailed);
     });
 
-    module('failing to get resource', {
-        setup: function() {
-            Request.prototype.ajax = function(params) {
+    module('failing to get resource');
+
+    asyncTest('unknown resource', function() {
+        var lastAjaxCall = null,
+            oldAjax = Request.ajax(function(params) {
                 var dfd = $.Deferred();
-                last_ajax_call = params;
+                lastAjaxCall = params;
                 setTimeout(function() {
                     dfd.fail();
                     params.error({
@@ -147,17 +144,68 @@ define([
                     });
                 }, 0);
                 return dfd;
-            };
-        },
-        teardown: function() {
-            Request.ajax = $.ajax;
-        }
+            }),
+            model = Example.models.get(17);
+
+        model.refresh().then(ajaxFailed, function(error, xhr) {
+            ok(!model._loaded);
+            start();
+        });
     });
 
-    asyncTest('unknown resource', function() {
-        var model = Example.models.get(17);
-        model.refresh().then(ajax_failed, function(error, xhr) {
-            ok(!model._loaded);
+
+    module('Model.save()');
+
+    // if a model is instantiated client-side and its 'id' is set client-side,
+    // then calling myModel.save() should still make a 'create' request, not an
+    // 'update'
+    asyncTest('correctly makes create request', function() {
+        var generatedId, self = this,
+            oldAjax = Request.ajax(function(params) {
+                self.ajaxFired = true;
+                equal(JSON.parse(params.data).id, generatedId);
+                params.success(params.data, 200, {});
+            }),
+            model = ExampleWithUuid({
+                id: (generatedId = uuid()),
+                required_field: 'foobar shutup..'
+            });
+
+        model.save().done(function() {
+            ok(self.ajaxFired);
+            Request.ajax(oldAjax);
+            start();
+        }).fail(function() {
+            ok(false, 'save failed');
+            Request.ajax(oldAjax);
+            start();
+        });
+    });
+
+    module('validation');
+
+    asyncTest('fail validation for missing field', function() {
+        var self = this,
+
+            // executing this function is equivalent to making a request to the
+            // server, but the test should fail before that ever happens
+            oldAjax = Request.ajax(function(params) {
+                ok(false, 'request should have failed validation before hitting the server');
+                params.success(params.data, 200, {});
+            }),
+
+            // omit the value for 'required_field'
+            model = Example({text_field: 'foobar'});
+
+        model.save().done(function() {
+            // something went wrong -- the request should have failed
+            ok(false, 'request should have failed to resolve b/c of validation error');
+            Request.ajax(oldAjax);
+            start();
+        }).fail(function() {
+            // we successfully got a validation failure
+            ok(true);
+            Request.ajax(oldAjax);
             start();
         });
     });
