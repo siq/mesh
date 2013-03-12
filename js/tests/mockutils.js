ca2069c12e74f4743f34a831f711dbc094865509
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-03-12 08:40:16 -0500

    augmented the js mockutils
    added the functions that lived in `auxl/test/mock/util.js`, and updated
    the `mockResource` function accordingly.

--- js/tests/mockutils.js
+++ js/tests/mockutils.js
@@ -6,9 +6,57 @@ define([
     'mesh/fields'
 ], function($, _, uuid, Class, fields) {
     var Xhr = Class.extend({
-            init:               function(stat) { this.status = stat || 200; },
-            getResponseHeader:  function() { return 'application/json'; }
+        init:               function(stat) { this.status = stat || 200; },
+        getResponseHeader:  function() { return 'application/json'; }
+    });
+
+    // we use a format for query params that's like json but not json
+    function pseudoJsonToObject(pseudoJson) {
+        var withoutCurlies = pseudoJson.replace(/^\{/, '').replace(/\}$/, ''),
+            items = withoutCurlies.split(','),
+            tuples = _.map(items, function(item) { return item.split(':'); });
+        return _.reduce(tuples, function(memo, item) {
+            memo[item[0]] = item[1];
+            return memo;
+        }, {});
+    }
+
+    function pseudoListToArray(pseudoList) {
+        return pseudoList.replace(/^\[/,'').replace(/\]$/,'').split(',');
+    }
+
+    // 'params' here is the object passed to the 'ajax' method of a request
+    function filterFromQuery(objects, params, attr) {
+        var filter;
+
+        attr = attr || 'name__icontains';
+        filter = params.data && params.data.query &&
+                 pseudoJsonToObject(params.data.query)[attr];
+
+        objects = !filter? _.toArray(objects) : _.filter(objects, function(m) {
+            return m.name.toLowerCase().indexOf(filter) >= 0;
         });
+
+        return _.map(objects, function(o) { return $.extend(true, {}, o); });
+    }
+
+    function stripUnlessIncluded(objects, params, check) {
+        var include = params.data && params.data.include?
+            pseudoListToArray(params.data.include) : [];
+        return _.map(objects, function(o) {
+            o = $.extend(true, {}, o); // just to be safe
+            return _.reduce(o, function(o, v, k) {
+                if (_.indexOf(check, k) > -1) {
+                    if (_.indexOf(include, k) > -1) {
+                        o[k] = v;
+                    }
+                } else {
+                    o[k] = v;
+                }
+                return o;
+            }, {});
+        });
+    }
 
     function mockResource(name, Resource, defaultResourceFixtures) {
         var hasUuid, id, defaultDelay = 0, delay = defaultDelay, fail = false,
@@ -26,23 +74,13 @@ define([
         window[name] = Resource;
 
         Resource.prototype.__requests__.query.ajax = reqHandlers.query = function(params) {
-            var query, dfd = $.Deferred(),
+            var dfd = $.Deferred(),
                 resources = [],
                 limit = params.data.limit || resourceFixtures.length,
                 offset = params.data.offset || 0,
                 shouldFail = fail;
 
-            query = eval('query = ' + (params.data.query || '{}'));
-
-            for (var i = offset; i < offset+limit; i++) {
-                if (query.integer_field__gt) {
-                    if (resourceFixtures[i].integer_field > query.integer_field__gt) {
-                        resources.push(_.extend({}, resourceFixtures[i]));
-                    }
-                } else {
-                    resources.push(_.extend({}, resourceFixtures[i]));
-                }
-            }
+            resources = filterFromQuery(resourceFixtures, params);
 
             setTimeout(function() {
                 if (shouldFail) {
@@ -62,21 +100,23 @@ define([
         };
 
         Resource.prototype.__requests__.get.ajax = reqHandlers.get = function(params) {
-            var obj, which = _.last(params.url.split('/')), shouldFail = fail;
+            var obj, objects,  shouldFail = fail,
+                which = _.last(params.url.split('/'));
 
             if (!hasUuid) {
                 which = +which;
             }
 
-            obj = $.extend(true, {}, _.find(resourceFixtures, function(e) {
-                return e.id === which;
-            }));
+            objects = filterFromQuery(resourceFixtures, params);
+            obj = _.find(objects, function(o) { return o.id === which; });
 
             setTimeout(function() {
                 if (shouldFail) {
                     params.error(Xhr(406));
+                } else if (!obj) {
+                    params.error(Xhr(404));
                 } else {
-                    params.success(obj, 200, Xhr());
+                    params.success($.extend(true, {}, obj), 200, Xhr());
                 }
             }, delay);
         };
