52ea8fcadc85d9ce03de5e3d941de9d99abda182
Ralph Smith <agentsmith@us.ibm.com>
2015-03-20 12:49:28 -0500

    update comments

--- examples/mello_world/mello/resources.py
+++ examples/mello_world/mello/resources.py
@@ -16,12 +16,13 @@ class MelloWorld(Resource):
 
     # Provide a custom request class that this resource will supports
     # This is similar to providing a new HTTP Method like GET but the method name will be `say_mello`
+    #   i.e. `response = client.execute('mello/1.0/melloworld', 'say_mello', None, {'name': name})`
+    # Because we're providing a custom request we will need a method in our controller with the
+    #   same name to handle this request.
     class say_mello:
         # Manually define an `endpoint`
         #   @param {http method} an http method to support
-        #   @param {string} name
-        #       this must match the rsource class name, controller method name, and request name
-        #       i.e. `response = client.execute('mello/1.0/melloworld', 'say_mello', None, {'name': name})`
+        #   @param {string} name (does not have to match the classname)
         endpoint = (GET, 'say_mello')
         schema = {'name': Text()}
         responses = {OK: {'message':Text(required=True, nonnull=True)}}
